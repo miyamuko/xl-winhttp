bec09691624cd418e7fe7c2069538ce49020c12e
MIYAMUKO Katsuyuki <miyamuko@gmail.com>
2012-01-13 22:48:16 +0900

    (build.bat): Made .sln file and Configuration property configurable.

--- src/xl-winhttp-helper/build.bat
+++ src/xl-winhttp-helper/build.bat
@@ -1,12 +1,22 @@
 @echo off
 setlocal
 
-set TARGET=Release
+if not "%1"=="" (
+  set TARGET=%1
+) else (
+  set TARGET=Release
+)
+
+if not "%2"=="" (
+  set SLN=%2
+) else (
+  set SLN=xl-winhttp-helper.sln
+)
 
 set _CD=%CD%
 cd %~dp0
 
-msbuild xl-winhttp-helper.sln /p:Configuration=%TARGET%
+msbuild %SLN% /p:Configuration=%TARGET%
 if not %ERRORLEVEL%==0 goto :end
 
 copy /Y %TARGET%\xl-winhttp-helper.dll ..\..\site-lisp\xl-winhttp\ffi
